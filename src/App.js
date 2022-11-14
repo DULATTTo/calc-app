4ae43a9bd4cd75d1cae9bd0d3d0d6a17e0c234f7
Dulat Abdurazakh <dulatttoc@gmail.com>
2022-11-14 21:47:07 +0600

    Structural and stylistic part is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import "./styles.css"
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="calc-grid">
+      <div className="output">
+        <div className="prev-val">123,123 +</div>
+        <div className="current-val">321,321</div>
+      </div>
+      <button>AC</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button>÷</button>
+      <button className="numbers">7</button>
+      <button className="numbers">8</button>
+      <button className="numbers">9</button>
+      <button>x</button>
+      <button className="numbers">4</button>
+      <button className="numbers">5</button>
+      <button className="numbers">6</button>
+      <button>-</button>
+      <button className="numbers">1</button>
+      <button className="numbers">2</button>
+      <button className="numbers">3</button>
+      <button>+</button>
+      <button className="numbers span-two">0</button>
+      <button>,</button>
+      <button>=</button>
     </div>
   );
 }
